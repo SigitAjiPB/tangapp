dd6240a388397dccd1443d65f7ad5581cccd42ae
unknown <sigitnj89@gmail.com>
2024-04-16 22:41:14 +0700

    membuat dropdown menu pada navbar

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,9 +1,18 @@
-const dropDownJs = document.getElementById('drop-down-js')
-const expand = document.getElementById('expand-nav-drop-down')
 
-
-expand.addEventListener('click', dropdown)
-
-function dropdown() {
-  dropDownJs.classList.toggle('hidden')
-}
+document.addEventListener('DOMContentLoaded', function() {
+  // Mendapatkan tombol dan konten dropdown
+  const dropDownJs = document.getElementById('drop-down-js')
+  const expand = document.getElementById('expand-nav-drop-down')
+ 
+  // Menampilkan dropdown saat tombol diklik
+  expand.addEventListener('click', function() {
+     dropDownJs.style.display = dropDownJs.style.display === 'none' ? 'block' : 'none';
+  });
+ 
+  // Menyembunyikan dropdown saat mengklik di luar area tombol dan dropdown
+  document.addEventListener('click', function(event) {
+     if (!expand.contains(event.target) && !dropDownJs.contains(event.target)) {
+       dropDownJs.style.display = 'none';
+     }
+  });
+ });
